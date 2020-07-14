6637636c5f4d9c5fccd0075e264df9b1f6d1eb90
bioeugenio <bioeugenio10@gmail.com>
2020-07-14 23:46:24 +0100

    DataLayer: menu, serving, course

---
+++ DataLayer/MenuData/Menu.cs
@@ -0,0 +1,51 @@
+﻿using Recodme.Rd.JadeRest.DataLayer.Base;
+using Recodme.Rd.JadeRest.DataLayer.RestaurantData;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Recodme.Rd.JadeRest.DataLayer.MenuData
+{
+    public class Menu : Entity
+    {
+        private DateTime _date;
+
+        [Required(ErrorMessage = "Required Attribute")]
+        public DateTime Date
+        {
+            get => _date;
+            set
+            {
+                _date = value;
+                RegisterChange();
+            }
+        }
+
+        [ForeignKey("Meal")]
+        public Guid MealId { get; set; }
+        public virtual Meal Meal { get; set; }
+
+        [ForeignKey("Restaurant")]
+        public Guid RestaurantId { get; set; }
+        public virtual Restaurant Restaurant { get; set; }
+
+        public virtual ICollection<Serving> Servings { get; set; }
+
+        public Menu(DateTime date, Guid mealId, Guid restaurantId)
+        {
+            _date = date;
+            MealId = mealId;
+            RestaurantId = restaurantId;
+        }
+
+        public Menu(Guid id, DateTime createdAt, DateTime updatedAt, bool isDeleted, DateTime date, Guid mealId, Guid restaurantId) : base(id, createdAt, updatedAt, isDeleted)
+        {
+            _date = date;
+            MealId = mealId;
+            RestaurantId = restaurantId;
+        }
+
+    }
+}
